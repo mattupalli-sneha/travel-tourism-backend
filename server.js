da22e5e16dc60e9379b45a9670d5424f0f0eace3
Sneha Mattupalli <mattupallisneha31@gmail.com>
2025-07-19 14:53:47 +0530

    Add root route

--- server.js
+++ server.js
@@ -147,3 +147,6 @@ app.delete('/admin/packages/:id', authenticateToken, async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
